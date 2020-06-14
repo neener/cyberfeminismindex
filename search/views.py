223662e8a0f0df98f60b2516ad528bfebed3f84a
Angeline <ammeitzler@gmail.com>
2020-06-13 21:48:03 -0400

    search categories

--- search/views.py
+++ search/views.py
@@ -8,11 +8,14 @@ from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 from django.shortcuts import render
 
 from index.models import IndexDetailPage
+from index.models import IndexCategory
 
 
 def search(request):
     search_query = request.GET.get('query', None)
     page = request.GET.get('page', 1)
+
+    search_categories = IndexCategory.objects.all()
 
     # Search
     if search_query:
@@ -36,4 +39,5 @@ def search(request):
     return render(request, 'search/search.html', {
         'search_query': search_query,
         'search_results': search_results,
+        'search_categories': search_categories,
     })
