aa8017ee72f74919e6ba5082873898d71d2f5444
Angeline <ammeitzler@gmail.com>
2020-07-18 10:57:30 -0400

    testing

--- cyberfem/static/js/cyberfem.js
+++ cyberfem/static/js/cyberfem.js
@@ -250,12 +250,14 @@ function internal_ligatures(selected_drawer) {
         var node = selected_drawer.children;
         for (i = 0; i < node.length; i++) { 
             for (j = 0; j < node[i].children.length; j++) {
+  				console.log(node[i].children[j])
                 if (node[i].children[j].nodeName == "A" && node[i].children[j].host == base_host) {
                     n.push(node[i].children[j])
                 }
             }
         }
         console.log(n)
+        console.log("about")
     }
 
     if (node.classList != "external_links") {
